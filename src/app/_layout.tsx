0aefab57a8f3bd3beaeac071e0099dfd99118579
Edis Leka <edisleka@yahoo.gr>
2026-04-23 16:55:12 +0300

    Fonts setup

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,31 @@
 import '@/root/global.css'
-import { Stack } from 'expo-router'
+import { useFonts } from 'expo-font'
+import { SplashScreen, Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
+import { useEffect } from 'react'
+
+SplashScreen.preventAutoHideAsync()
 
 const InitialLayout = () => {
+  const [fontsLoaded] = useFonts({
+    'sans-regular': require('@/assets/fonts/PlusJakartaSans-Regular.ttf'),
+    'sans-medium': require('@/assets/fonts/PlusJakartaSans-Medium.ttf'),
+    'sans-bold': require('@/assets/fonts/PlusJakartaSans-Bold.ttf'),
+    'sans-semibold': require('@/assets/fonts/PlusJakartaSans-SemiBold.ttf'),
+    'sans-extrabold': require('@/assets/fonts/PlusJakartaSans-ExtraBold.ttf'),
+    'sans-light': require('@/assets/fonts/PlusJakartaSans-Light.ttf'),
+  })
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded])
+
+  if (!fontsLoaded) {
+    return null
+  }
+
   return (
     <>
       <StatusBar style='auto' />
