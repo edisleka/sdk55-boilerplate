08af98f9cb7d3409dc843875d61926820d732dbc
Edis Leka <edisleka@yahoo.gr>
2026-04-23 14:09:38 +0300

    Nativewind v5 setup

--- src/screens/onboarding/Onboarding.tsx
+++ src/screens/onboarding/Onboarding.tsx
@@ -2,8 +2,8 @@ import { Text, View } from 'react-native'
 
 export default function OnboardingScreen() {
   return (
-    <View>
-      <Text>Onboarding Now</Text>
+    <View className='flex-1 items-center justify-center'>
+      <Text className='text-2xl font-bold'>Onboarding Now</Text>
     </View>
   )
 }
